240e6aff344a2506490d5be188ada97bc1ccd90e
Borislav Dimitrov <borko_999@mail.bgm>
2022-08-16 11:07:52 +0300

    Add levels

--- LevelManager.gd
+++ LevelManager.gd
@@ -1,17 +1,6 @@
 extends Node
 
-var levelDificulty = 0
-var levelTypeIndex = 0
-
-var tileDestroyerEasy = {
-	0 : [2, 0.3, 50, 5],
-	1 : [3, 0.3, 5, 9]
-}
-var tileDestroyerHard = {
-	0 : [4, 0.3, 50, 5],
-	1 : [5, 0.3, 5, 9]
-}
-var tileDestroyerDiff = [tileDestroyerEasy, tileDestroyerHard]
+var completedLevelsCount = 0
 
 onready var levels = [
 	preload("res://Level/ClassicLevel.tscn"),
@@ -23,97 +12,31 @@ func _ready():
 	StartNextLevel()
 	
 func HandleLevelWon():
-	levelTypeIndex += 1
+	completedLevelsCount += 1
 	StartNextLevel()
 	
 func StartNextLevel():
-	levelDificulty = levelTypeIndex / levels.size()
-	var level = levels[2]
-#	var level = levels[levelTypeIndex % levels.size()]
+	var levelDificulty = completedLevelsCount / levels.size()
+	var level = levels[completedLevelsCount % levels.size()]
 	var level_path = level.resource_path
 	
 	for child in get_children():
-		child.queue_free()
+		if child.name != "LevelDificulties":
+			child.queue_free()
 	
 	if level_path == "res://Level/ClassicLevel.tscn":
 		var scene = level.instance()
 		add_child(scene)
-		scene.initializeClassicLevel(2, 0.7)
+		var levelDif = $LevelDificulties.tileClassicLevels[levelDificulty]
+		scene.initializeClassicLevel(levelDif[0], levelDif[1])
 	elif level_path == "res://Level/LevelTileCount.tscn":
 		var scene = level.instance()
 		add_child(scene)
-		scene.initializeTileCounter(2, 0.0, 30, 3)
-		# EASY
-		# 2, 0.3, 50, 5
-		# 3, 0.3, 40, 5
-		# 4, 0.3, 30, 5
-		# 5, 0.3, 20, 5
-		# 6, 0.3, 15, 5
-		# 7, 0.3, 10, 5
+		var levelDif = $LevelDificulties.tileCounterLevels[levelDificulty]
+		scene.initializeTileCounter(levelDif[0], levelDif[1], levelDif[2], levelDif[3])
 		
-		# NORMAL
-		# 2, 0.3, 50, 7
-		# 3, 0.3, 40, 7
-		# 4, 0.3, 30, 7
-		# 5, 0.3, 20, 7
-		# 6, 0.3, 15, 7
-		# 7, 0.3, 10, 7
-		
-		# HARD
-		# 2, 0.1, 50, 5
-		# 3, 0.1, 50, 5
-		# 4, 0.1, 40, 5
-		# 5, 0.1, 30, 5
-		# 6, 0.1, 25, 5
-		# 7, 0.1, 20, 5
-		
-		# EXPERT
-		# 2, 0.0, 60, 5
-		# 3, 0.0, 50, 5
-		# 4, 0.0, 50, 3
-		# 5, 0.0, 40, 4
-		# 6, 0.0, 35, 3
-		# 7, 0.0, 30, 3
 	elif level_path == "res://Level/LevelTileDestroyer.tscn":
 		var scene = level.instance()
 		add_child(scene)
-		var levelDif = tileDestroyerDiff[0][1]
+		var levelDif = $LevelDificulties.tileDestroyerLevels[levelDificulty]
 		scene.initializeTileDestroyer(levelDif[0], levelDif[1], levelDif[2], levelDif[3])
-		# EASY
-		# 2, 0.3, 3, 10
-		# 3, 0.3, 5, 9
-		# 4, 0.3, 5, 10
-		# 5, 0.3, 7, 7
-		# 6, 0.3, 5, 7
-		# 7, 0.3, 9, 6 maybe?
-		
-		# NORMAL
-		# 2, 0.2, 6, 10
-		# 3, 0.2, 9, 9
-		# 4, 0.2, 9, 7
-		# 5, 0.2, 9, 6
-		# 6, 0.2, 9, 6
-		# 7, 0.2, 8, 6
-		
-		# HARD
-		# 2, 0.1, 6, 10
-		# 3, 0.1, 8, 8
-		# 4, 0.1, 8, 7
-		# 5, 0.1, 7, 7
-		# 6, 0.1, 7, 6
-		# 7, 0.1, 10, 5
-		
-		# EXPERT
-		# 2, 0.0, 6, 10
-		# 3, 0.0, 7, 9
-		# 4, 0.0, 7, 7
-		# 5, 0.0, 9, 6
-		# 6, 0.0, 11, 5
-		# 7, 0.0, 13, 4
-		
-		
-		
-		
-		
-		
-		
